f9a656aa6fd71d08e093b7884bfc8332eb0c3b28
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-26 20:33:42 +0530

    add function setActiveSlide

--- script.js
+++ script.js
@@ -10,3 +10,8 @@ setBgToBody();
 function setBgToBody() {
   body.style.backgroundImage = slides[activeSlide].style.backgroundImage;
 }
+
+function setActiveSlide() {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+}

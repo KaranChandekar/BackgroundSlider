67b875b7c4a45118955e27fa0bedc881abe71d42
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-26 20:27:51 +0530

    initialize a variable

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ const body = document.body;
 const slides = document.querySelectorAll(".slide");
 const leftBtn = document.getElementById("left");
 const rightBtn = document.getElementById("right");
+
+let activeSlide = 0;

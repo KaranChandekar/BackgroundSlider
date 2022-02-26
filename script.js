c8f399e0b81a63d7aa448d615003d17626e4e6fb
Karan Chandekar <karanchandekar431@gmail.com>
2022-02-26 20:27:19 +0530

    select elements with js

---
+++ script.js
@@ -0,0 +1,4 @@
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
